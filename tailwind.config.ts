7dbf5163b8cd87dd00b9e2286d483f641ac4d4ef
chenmu2024 <zhongqiaosheng@gmail.com>
2025-07-31 05:58:01 +0200

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,53 @@
+import type { Config } from "tailwindcss"
+import defaultConfig from "shadcn/ui/tailwind.config"
+
+const config: Config = {
+  ...defaultConfig,
+  content: [
+    ...defaultConfig.content,
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    ...defaultConfig.theme,
+    extend: {
+      ...defaultConfig.theme.extend,
+      colors: {
+        ...defaultConfig.theme.extend.colors,
+        slate: {
+          50: "#F8FAFC",
+          100: "#F1F5F9",
+          200: "#E2E8F0",
+          300: "#CBD5E1",
+          400: "#94A3B8",
+          500: "#64748B",
+          600: "#475569",
+          700: "#334155",
+          800: "#1E293B",
+          900: "#0F172A",
+          950: "#020617",
+        },
+        blue: {
+          500: "#3B82F6",
+          600: "#2563EB",
+          400: "#60A5FA",
+          300: "#93C5FD",
+        },
+        green: {
+          500: "#22C55E",
+          600: "#16A34A",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [...defaultConfig.plugins, require("@tailwindcss/typography"), require("tailwindcss-animate")],
+}
+
+export default config
